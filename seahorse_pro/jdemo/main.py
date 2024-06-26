ef00cb90fe56e9b06fa95de6e1e86ff65d17c727
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-06-26 09:18:49 +0900

    fix

--- seahorse_pro/jdemo/main.py
+++ seahorse_pro/jdemo/main.py
@@ -5,11 +5,11 @@ import classic
 Direction = "sell"  # buy sell 初始方向
 Magic = 0  # 魔术手
 Deviation = 30  # 滑点
-CurrencySuffix = "c"  # 货币后缀
+CurrencySuffix = "m"  # 货币后缀
 InitialVolume = 0.01  # 初始volume
 IncreaseMultiple = 0.01  # 加仓倍数
 Symbol = "EURUSD"  # 基础
-Interval = 10  # 加仓间隔
+Interval = 5  # 加仓间隔
 TimeInterval = 30  # 时间间隔 default 30分
 #########################################################
 
