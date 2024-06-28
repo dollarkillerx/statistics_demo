42180266e91f37997fd643d116ffec692d57a08c
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-06-29 00:07:06 +0900

    fix

--- followme/sdk/release.py
+++ followme/sdk/release.py
@@ -1,5 +1,5 @@
 import followme_sdk
 
 if __name__ == '__main__':
-    sdk = followme_sdk.FollowMeSDK("https://followme.dollarkiller.com","FollowMe","C:\\Users\\Administrator\\Desktop\\MT3\\terminal64.exe")
+    sdk = followme_sdk.FollowMeSDK("http://followme1.cpx.com","FollowMe","C:\\Users\\Administrator\\Desktop\\MT3\\terminal64.exe")
     sdk.release()
